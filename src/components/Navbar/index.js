542f3e94661d89d73fe464ac55f270a0764aa29f
Brian Sturgis <sturujisu@gmail.com>
2021-07-26 11:22:38 -0700

    update project with More build to the navbar components as well as add router dom and styled components dependencies

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,11 +1,22 @@
 import React from 'react'
+import {Nav,NavbarContainer,NavLogo} from './NavbarElements';
 
 const Navbar = () => {
 	return (
 		<>
 			<Nav>
 				<NavbarContainer>
-					<NavLogo>Allora</NavLogo>
+					<NavLogo to="/">Allora</NavLogo>
+					<MobileIcon>
+						<FaBars/>
+					</MobileIcon>
+					<NavMenu>
+						<NavItem>
+							<NavLinks to="about"
+					
+							
+						</NavItem>
+					</NavMenu>
 				</NavbarContainer>
 			</Nav>
 		</>
