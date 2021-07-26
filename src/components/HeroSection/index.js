0f2bd641632f893bd585aa1b6d20af64a3a47835
Brian Sturgis <sturujisu@gmail.com>
2021-07-26 15:53:02 -0700

    update HeroSection with basic styling

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 const HeroSection = () => {
 	return (
-		<>
-			
-		</>
+		<HeroContainer id="home">
+			<HeroBg>
+				<VideoBg autoPlay loop muted src={Video} type='video/mp4'></VideoBg>
+			</HeroBg>
+		</HeroContainer>
 	)
 }
 
