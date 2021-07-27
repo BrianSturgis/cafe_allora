88a413dc7dbc7a9f18e3e2f9a1ed370bf2028ac0
Brian Sturgis <sturujisu@gmail.com>
2021-07-27 14:19:32 -0700

    fixes bug in index.js

--- src/components/InfoSection/index.js
+++ src/components/InfoSection/index.js
@@ -14,7 +14,7 @@ const InfoSection = () => {
 								<Heading>Heading</Heading>
 								<Subtitle>SubTitle</Subtitle>
 								<BtnWrap>
-									<Button to="home">Button</Button>
+									<Button to="home"></Button>
 								</BtnWrap>
 							</TextWrapper>
 						</Column1>
