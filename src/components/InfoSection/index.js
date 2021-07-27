9d577b85fdbdffd7a9e487c99be60b925b53f62a
Brian Sturgis <sturujisu@gmail.com>
2021-07-27 11:11:34 -0700

    atempt to a address a  bug in idex.js

--- src/components/InfoSection/index.js
+++ src/components/InfoSection/index.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import { Button } from 'react-scroll';
+import {InfoContainer, InfoWrapper, InfoRow, Column1, Column2, TextWrapper, TopLine, Heading, Subtitle, BtnWrap, ImgWrap, Img} from './InfoElements'
 
 const InfoSection = () => {
 	return (
 		<>
-			<InfoContainer id={id}>
+			<InfoContainer>
 				<InfoWrapper>
 					<InfoRow>
 						<Column1>
 							<TextWrapper>
 								<TopLine>TopLine</TopLine>
 								<Heading>Heading</Heading>
-								<SubTitle>SubTitle</SubTitle>
+								<Subtitle>SubTitle</Subtitle>
 								<BtnWrap>
 									<Button to="home">Button</Button>
 								</BtnWrap>
 							</TextWrapper>
 						</Column1>
+						<Column2>
+							<ImgWrap>
+							<Img/>
+							</ImgWrap>
+						</Column2>
 					</InfoRow>
 				</InfoWrapper>
 			</InfoContainer>
