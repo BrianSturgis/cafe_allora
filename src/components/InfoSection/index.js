28f465e2b051099764bc75f8b7a1c4f65f4f02d2
Brian Sturgis <sturujisu@gmail.com>
2021-07-28 08:25:09 -0700

    make small syntax fixes

--- src/components/InfoSection/index.js
+++ src/components/InfoSection/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Button } from '../ButtonElements';
 import {InfoContainer, InfoWrapper, InfoRow, Column1, Column2, TextWrapper, TopLine, Heading, Subtitle, BtnWrap, ImgWrap, Img} from './InfoElements';
 
-const InfoSection = ({lightBg,id,imgStart,topLine,lightText,headline,darkText,description,buttonLabel,img,alt},primary, dark, dark2) => {
+const InfoSection = ({lightBg,id,imgStart,topLine,lightText,headLine,darkText,description,buttonLabel,img,alt},primary, dark, dark2) => {
 	return (
 		<>
 			<InfoContainer lightBg={lightBg} id={id}>
@@ -11,7 +11,7 @@ const InfoSection = ({lightBg,id,imgStart,topLine,lightText,headline,darkText,de
 						<Column1>
 							<TextWrapper>
 								<TopLine>{topLine}</TopLine>
-								<Heading lightText = {lightText}>{headline}</Heading>
+								<Heading lightText = {lightText}>{headLine}</Heading>
 								<Subtitle darkText={darkText}>{description}</Subtitle>
 								<BtnWrap>
 									<Button to="home"
